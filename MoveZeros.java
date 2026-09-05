72d581a448c791726e638884f292610fad020ec4
Praveen M <praveenmpravee7242@gmail.com>
2026-09-05 16:22:52 +0530

    updated

--- MoveZeros.java
+++ MoveZeros.java
@@ -1,39 +1,52 @@
 class MoveZeros {
     public static void moveZeroes(int[] nums) {// { 0, 1, 0, 3, 1, 2 }
+
         int left = 0;
-        int right = left + 1;
-        int length1 = (nums.length) - 1;
 
-        if (nums.length == 0 || nums.length == 1) {
-            System.out.println(nums[0]);
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
 
-        }
-
-        while (left < nums.length - 1 && right < nums.length) {
-            if (nums[left] == 0 && nums[right] == 0) {
-                right++;
             }
-
-            else if (nums[left] == 0 && nums[right] != 0) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                right++;
-                left++;
-
-            } else if (nums[left] != 0 && nums[right] != 0) {
-                right++;
-                left++;
-            } else if (nums[left] != 0 && nums[right] == 0) {
-                right++;
-                left++;
-            }
-
         }
-
+        /*
+         * int right = left + 1;
+         * int length1 = (nums.length) - 1;
+         * 
+         * if (nums.length == 0 || nums.length == 1) {
+         * System.out.println(nums[0]);
+         * 
+         * }
+         * 
+         * while (left < nums.length - 1 && right < nums.length) {
+         * if (nums[left] == 0 && nums[right] == 0) {
+         * right++;
+         * }
+         * 
+         * else if (nums[left] == 0 && nums[right] != 0) {
+         * int temp = nums[left];
+         * nums[left] = nums[right];
+         * nums[right] = temp;
+         * right++;
+         * left++;
+         * 
+         * } else if (nums[left] != 0 && nums[right] != 0) {
+         * right++;
+         * left++;
+         * } else if (nums[left] != 0 && nums[right] == 0) {
+         * right++;
+         * left++;
+         * }
+         * 
+         * }
+         */
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
         }
+
     }
 
     public static void main(String[] args) {
