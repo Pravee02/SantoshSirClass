7ea2468208e2fe68db6a4d2d42426498570517cb
Praveen M <praveenmpravee7242@gmail.com>
2026-09-07 12:27:04 +0530

    updated

--- Palindrome.java
+++ Palindrome.java
@@ -1,17 +1,25 @@
 import java.util.*;
 
-public class ReverseInt {
+public class Palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
+
         int num = sc.nextInt();
         int reverse = 0;
+        int original = num;
         while (num > 0) {
             int digit = num % 10;
             reverse = reverse * 10 + digit;
             num = num / 10;
-
         }
-        System.out.println(reverse);
+        System.out.println("is it palindrome true or false ");
+        boolean result = false;
+        if (reverse == original) {
+            result = true;
+            System.out.println(result);
+        } else {
+            System.out.println(result);
+        }
     }
 }
