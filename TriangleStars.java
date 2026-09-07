3b4a8699807ffcb03686a62a95e2d2331357a97f
Praveen M <praveenmpravee7242@gmail.com>
2026-09-07 21:56:18 +0530

    updated

--- TriangleStars.java
+++ TriangleStars.java
@@ -1,12 +1,14 @@
 public class TriangleStars {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) { // number of rows for(int i = 1 ; i<=5;i++){
-            for (int j = i; j <= 5; j++) { // number of stars for(j = 1 ; j <= i ; j++)}
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        /*
+         * for (int i = 1; i <= 5; i++) {
+         * for (int j = i; j <= 5; j++) { // number of stars for(j = 1 ; j <= i ; j++)}
+         * System.out.print("*");
+         * }
+         * System.out.println();
+         * }
+         */
 
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
