5d1ee04c4bfe1cc4d42d6f77dd1381f396a9f84d
Praveen M <praveenmpravee7242@gmail.com>
2026-09-09 11:26:40 +0530

    new filr

--- StartsTriangle.java
+++ StartsTriangle.java
@@ -62,15 +62,5 @@ public class StartsTriangle {
         System.out.println();
         System.out.println();
 
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5 - i; j++) {
-                System.out.println(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-
-            System.out.println();
-        }
     }
 }
