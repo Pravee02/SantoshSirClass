56501ca3003217c86bde26f1ea262b7085064610
Praveen M <praveenmpravee7242@gmail.com>
2026-09-09 12:50:46 +0530

    new file

--- Methods2.java
+++ Methods2.java
@@ -8,15 +8,44 @@ class Methodss {
             return y;
     }
 
-    public class Methods2 {
+    void printSum(int a, int b) {
+        System.out.println(a + b);
+    }
 
-        public static void main(String[] args) {
+    int getNumber() {
+        return 100;
+    }
 
-            Methodss obj = new Methodss();
-            int a = 10;
-            int b = 20;
-            int result = obj.max(a, b);
-            System.out.println(result);
-        }
+    void display() {
+        System.out.println("Hello");
+    }
+
+}
+
+public class Methods2 {
+
+    public static void main(String[] args) {
+
+        // obj 1
+        Methodss obj = new Methodss();
+
+        int l = 10;
+        int m = 20;
+        int result = obj.max(l, m);
+        System.out.println(result);
+
+        // obj 2
+        Methodss obj1 = new Methodss();
+        obj1.printSum(l, m);
+
+        // obj 3
+        Methodss obj2 = new Methodss();
+        int result1 = obj2.getNumber();
+        System.out.println(result1);
+
+        // obj 4
+        Methodss obj3 = new Methodss();
+        obj3.display();
+
     }
 }
