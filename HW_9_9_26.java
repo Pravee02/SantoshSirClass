935789d9e36c5523c3b389e281031f7baffa2301
Praveen M <praveenmpravee7242@gmail.com>
2026-09-09 21:38:08 +0530

    updated files

--- HW_9_9_26.java
+++ HW_9_9_26.java
@@ -1,4 +1,6 @@
 class Square {
+
+    // square of numbers
     int SquareOfNumbers(int n) {
         if (n <= 0) {
             return 0;
@@ -7,10 +9,61 @@ class Square {
             return result;
         }
     }
+
+    // check even or add
+    void EvenOrAdd(int n) {
+        if (n <= 0) {
+            System.out.println("0");
+        } else {
+            if (n % 2 == 0)
+                System.out.println(n + " is an even number ");
+            else
+                System.out.println(n + " is an odd number ");
+        }
+    }
+
+    // max of three numbers
+    void CheckMaxOfThree(int a, int b, int c) {
+        if (a == 0 && b == 0 && c == 0) {
+            System.out.println("0");
+        } else {
+
+            if (a > b && a > c) {
+                System.out.println(a + " is greater");
+            } else if (b > c) {
+                System.out.println(b + " is greater");
+            } else {
+                System.out.println(c + " is greater");
+            }
+        }
+    }
+
+    // cout the digit of n number
+    void CountTheDigit(int n) {
+        int count = 0;
+        int original = n;
+        if (n == 0) {
+            System.out.println("the total counts  is 0");
+        }
+
+        else {
+            while (n > 0) {
+                int digit = n % 10;
+                count += 1;
+                n = n / 10;
+            }
+
+        }
+        System.out.println("the count of " + original + " is " + count);
+    }
+
 }
+
+    
 
 public class HW_9_9_26 {
 
+    // square of numbers
     static int SquareOfNumber(int num) {
         if (num <= 0) {
             return 0;
@@ -20,13 +73,88 @@ public class HW_9_9_26 {
         }
     }
 
+    // check even or add
+    static void EvenOrAdd(int n) {
+        if (n <= 0) {
+            System.out.println("0");
+        } else {
+            if (n % 2 == 0)
+                System.out.println(n + " is an even number ");
+            else
+                System.out.println(n + " is an odd number ");
+        }
+    }
+
+    // max of three numbers
+    static void CheckMaxOfThree(int a, int b, int c) {
+        if (a == 0 && b == 0 && c == 0) {
+            System.out.println("0");
+        } else {
+
+            if (a > b && a > c) {
+                System.out.println(a + " is greater");
+            } else if (b > c) {
+                System.out.println(b + " is greater");
+            } else {
+                System.out.println(c + " is greater");
+            }
+        }
+    }
+
+    // cout the digit of n number
+    static void CountTheDigit(int n) {
+        int count = 0;
+        int original = n;
+        if (n == 0) {
+            System.out.println("the total counts  is 0");
+        }
+
+        else {
+            while (n > 0) {
+                int digit = n % 10;
+                count += 1;
+                n = n / 10;
+            }
+
+        }
+        System.out.println("the count of " + original + " is " + count);
+    }
+
     public static void main(String[] args) {
+        // without object square of numbers
         int a = 5;
+        int b = 10;
+        int c = 30;
+        int num = 5364;
         int result = SquareOfNumber(a);
         System.out.println("square of " + a + " is " + result);
 
+        // with object square of numbers
         Square obj = new Square();
         int result1 = obj.SquareOfNumbers(a);
-        System.out.println("square of " + a + " using object creation is " + result);
+        System.out.println("square of " + a + " using object creation is " + result1);
+        System.out.println();
+
+        // with object check even or add
+        obj.EvenOrAdd(a);
+
+        // without object check even or add
+        EvenOrAdd(a);
+        System.out.println();
+
+        // with object check max of three numbers
+        obj.CheckMaxOfThree(a, b, c);
+
+        // without object check max of three numbers
+        CheckMaxOfThree(a, b, c);
+        System.out.println();
+
+        // with object to count the numbers
+        obj.CountTheDigit(num);
+
+        // without object to count the numbers
+        CountTheDigit(num);
+        System.out.println();
+
     }
 }
