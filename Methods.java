8ef1b3167e990cf02d74b7ed6bccdc486d5cb649
Praveen M <praveenmpravee7242@gmail.com>
2026-09-09 11:28:56 +0530

    new filr

---
+++ Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+
+    static void display() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args)
+
+    {
+        display();
+    }
+}
