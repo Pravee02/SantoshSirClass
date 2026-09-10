9dd881284b80c2269ac55fbbad85383328392616
Praveen M <praveenmpravee7242@gmail.com>
2026-09-10 11:26:44 +0530

    new file

--- InstanceVoid.java
+++ InstanceVoid.java
@@ -11,6 +11,12 @@ public class InstanceVoid {
         }
     }
 
+    // instance non void
+    boolean checkEvenOdd1(int n) {
+
+        return n % 2 == 0;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a number to check even or add");
@@ -18,5 +24,8 @@ public class InstanceVoid {
         InstanceVoid obj = new InstanceVoid();
         obj.checkEvenOdd(n);
 
+        // non void
+        boolean result = obj.checkEvenOdd1(n);
+        System.out.println(result);
     }
 }
